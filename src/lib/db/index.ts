3104edeaca86fbbf819f0268f329a8a655811e54
rfinnbogason <rfinnbogason@gmail.com>
2026-04-21 11:27:14 -0700

    Fix: enable SSL for postgres connection

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,6 +2,6 @@ import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
 import * as schema from "./schema"
 
-const client = postgres(process.env.DATABASE_URL ?? "postgresql://user:password@ep-build-placeholder.us-east-1.aws.neon.tech/neondb")
+const client = postgres(process.env.DATABASE_URL ?? "postgresql://user:password@ep-build-placeholder.us-east-1.aws.neon.tech/neondb", { ssl: "require" })
 export const db = drizzle(client, { schema })
 export * from "./schema"
